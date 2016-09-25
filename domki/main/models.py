2fa8f216888201109e2d489dd6633fb2a772b7a2
BartoszCki <bartosz.cierocki@gmail.com>
2016-09-25 22:21:37 +0200

    Models 0.0.1 pre-alpha

--- domki/main/models.py
+++ domki/main/models.py
@@ -1,31 +1,137 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 # Create your models here.
 class Home(models.Models):
-    TYPES = (
-        (1,"one_room"),
-        (2,"flat"),
-        (3,"apartment"),
-        (4,"house"),
-        (5,"place_in_room"),
+    APARTMENT_TYPES = (
+        (1, _("Studio")),
+        (2, _("Apartment")),
+        (3, _("House")),
+        (4, _("Twin house")),
+        (5, _("Shared room")),
+        (6, _("Shared apartment")),
     )
 
-    city = models.CharField(max_length=10, blank=True)
-    street = models.CharField(max_length=10, blank=True)
-    postal_code = models.CharField(max_length=10, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="homes")
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    # TODO: do some magic update after on new Review
+    rating = models.DecimalField(decimal_places=2)
+
+    # PRICES AND AVAILABILITY
+    is_available_now = models.BooleanField(default=False)
+    price_per_night = models.CharField(max_length=10, blank=False)
+    price_per_week = models.CharField(max_length=10, blank=False)
+    price_per_month = models.CharField(max_length=10, blank=False)
+    price_per_year = models.CharField(max_length=10, blank=True)
+
+    # ADDRESS
+    city = models.CharField(max_length=100, blank=False)
+    street = models.CharField(max_length=100, blank=False)
+    postal_code = models.CharField(max_length=10, blank=False)
     building_number = models.CharField(max_length=10, blank=True)
     apartment_number = models.CharField(max_length=10, blank=True)
-    type_of_rent = models.CharField(choices=TYPES, blank=false)
 
-#     equipment
-    washing = models.BooleanField(default=False)
+    # APARTMENT INFO
+    type_of_rent = models.CharField(choices=APARTMENT_TYPES, blank=False)
+    floor = models.PositiveIntegerField(blank=False)
+    floors_in_building = models.PositiveIntegerField(blank=False)
+
+    # TODO: do some auto counting magic https://www.stavros.io/posts/how-replace-django-model-field-property/
+    # ROOM COUNTS
+    living_rooms = models.PositiveIntegerField(blank=False)
+    bedrooms = models.PositiveIntegerField(lank=False)
+    bathrooms = models.PositiveIntegerField(blank=False)
+    toilets = models.PositiveIntegerField(blank=False)
+    kitchens = models.PositiveIntegerField(blank=False)
+    garages = models.PositiveIntegerField(blank=False)
+
+    # RULES
+    PET_POLICY = (
+        (0, _("No pets allowed")),
+        (1, _("Dogs only")),
+        (2, _("Cats only")),
+        (3, _("Cats and dogs")),
+    )
+    pet_policy = models.CharField(choices=PET_POLICY, blank=False)
+    smoking_allowed = models.BooleanField(defaut=False)
+    guests_overnight = models.BooleanField(defaut=False)
+
+
+class Room(models.Models):
+    SHAPES = (
+        (0, _("Square")),
+        (1, _("Rectangle")),
+        (2, _("Other"))
+    )
+    home = models.ForeignKey(Home, related_name="homes")
+    area_in_sq_m = models.PositiveIntegerField(decimal_places=2, blank=False)
+    shape = models.CharField(choices=SHAPES, blank=False)
+    angled_ceiling = models.BooleanField(default=False)
+    windowless = models.BooleanField(defaut=False)
+
+
+class Bedroom(Room):
+    bed = models.BooleanField(default=False)
+
+
+class LivingRoom(Room):
+    fireplace = models.BooleanField(default=False)
+
+
+class Bathroom(Room):
     shower = models.BooleanField(default=False)
     bath = models.BooleanField(default=False)
-    oven = models.BooleanField(default=False)
-    bed = models.BooleanField(default=False)
-    fridge = models.BooleanField(default=False)
-    freezer = models.BooleanField(default=False)
+    washing_machine = models.BooleanField(default=False) # TODO: w sumie to dlaczego by nie w kuchni/garażu...
     dryer = models.BooleanField(default=False)
 
-# equipment ends here
+
+class Toilet(Room):
+    pass
+
+
+class Kitchen(Room):
+    stove = models.BooleanField(default=False)
+    oven = models.BooleanField(default=False)
+    fridge = models.BooleanField(default=False)
+    freezer = models.BooleanField(default=False)
+
+
+class Garage(Room):
+    BAYS = (
+        (1, "1"),
+        (2, "2"),
+        (3, "3+")
+    )
+    width_in_m = models.CharField(max_length=10, blank=False)
+    depth_in_m = models.CharField(max_length=10, blank=False)
+    bays = models.CharField(choices=BAYS, blank=False)
+
+
+class User(AbstractUser):
+    city = models.CharField(max_length=100, blank=False)
+    street = models.CharField(max_length=100, blank=False)
+    postal_code = models.CharField(max_length=10, blank=False)
+    building_number = models.CharField(max_length=10, blank=True)
+    apartment_number = models.CharField(max_length=10, blank=True)
+    agency_employee = models.BooleanField(default=False)
+
+
+class Agency(models.Models):
+    city = models.CharField(max_length=100, blank=False)
+    street = models.CharField(max_length=100, blank=False)
+    postal_code = models.CharField(max_length=10, blank=False)
+    building_number = models.DecimalField(max_digits=10, blank=True)
+    apartment_number = models.DecimalField(max_digits=10, blank=True)
+
+
+class Review(models.Models):
+    user = models.ForeignKey(User)
+    rating = models.PositiveIntegerField()
+    review_text = models.TextField()
+
+class Image(models.Models):
+    room = models.ForeignKey(Room)
+    title = models.ImageField()  # TODO: add arguments
