bae1e8d0beede5817a6315e573674760cb4172b3
n3st0r <piotr.dobrysiak@gmail.com>
2016-11-10 19:47:48 +0100

    Added first test

---
+++ tests/test_apartment.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from app.models.apartment import Apartment
+
+
+class TestApartment(TestCase):
+    "Show setup and teardown"
+
+    def setUp(self):
+        Apartment.objects.create(name="pierwszy", floor=3)
+
+    def test_apartment(self):
+        """Apartment has the same params, as created"""
+        obj = Apartment.objects.get(name="pierwszy")
+        self.assertEqual(obj.floor, 3)
