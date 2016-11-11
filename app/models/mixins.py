30aa637fb0ca99b9bb00472e5b340a49a0b7f736
n3st0r <piotr.dobrysiak@gmail.com>
2016-11-11 05:37:45 +0100

    UUID as primary key

--- app/models/mixins.py
+++ app/models/mixins.py
@@ -1,7 +1,13 @@
+import uuid
 from django.db import models
 
 
 class Simple(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     name = models.CharField('Nazwa', max_length=64)
 
     class Meta:
