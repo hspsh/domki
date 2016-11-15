d8a6ed273d6a6cda7dae0f1df49e84ae9dd88f28
Mjure <mateusz.jurewicz@gmail.com>
2016-11-15 23:47:20 +0100

    Added basic single apartment view

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 # We are adding a URL called /home
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^apartment$', views.apartment, name='apartment'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request,'app/index.html')
+
+def apartment (request):
+    return render(request,'app/apartment.html')
